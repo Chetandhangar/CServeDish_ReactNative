69ca8f472473241b14c07bcf6ec21a1506bbf22a
Chetan Dhangar <chetansdhangar@gmail.com>
2020-12-26 21:18:43 +0530

    feat- React Navigation stack

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,45 +1,57 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import PropTypes from 'prop-types'
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishDetailComponent';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 /**
 * @auhor
 * @class Main
 **/
+
+const MenuNavigator = createStackNavigator();
+
+function MenuNavigatorScreen(){
+    return(
+      <MenuNavigator.Navigator
+       initialRouteName="Menu"
+       screenOptions={{
+           headerStyle:{
+               backgroundColor:"#512DA8"
+           },
+           headerTintColor:"#fff",
+           headerTitleStyle:{
+               color:"#fff"           }
+       }}
+     >
+         <MenuNavigator.Screen
+         name="Menu"
+         component={Menu}
+         />
+         <MenuNavigator.Screen
+         name="Dishdetail"
+         component={Dishdetail}
+         options={{headerTitle:"Dish Detail"}}
+         />
+
+         
+
+      </MenuNavigator.Navigator>
+    );
+}
+
 class Main extends Component {
-    constructor(props){
-        super(props);
-
-        this.state={
-            dishes : DISHES,
-            selectedDish : null
-        }
-    }
-
-    onDishSelect (dishId) {
-        this.setState({selectedDish : dishId});
-    }
  
  render(){
-  const { container } = styles
+
   return(
-      <View>
-          <Menu dishes={this.state.dishes} onPress= {(dishId) => this.onDishSelect(dishId)}  />
-          <Dishdetail dish={this.state.dishes.filter((dish)=>dish.id === this.state.selectedDish)[0]}/>
-      </View>
+      <NavigationContainer>
+          <MenuNavigatorScreen />
+      </NavigationContainer>
    
    )
   }
  }
 
 
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-  }
-})
+
 export default Main
