bbff31bd5c5b8e1c408a542db9f5966ffe4c03cf
Chetan Dhangar <chetansdhangar@gmail.com>
2020-12-30 11:55:24 +0530

    feat- Make Favorite Dish

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, ScrollView, FlatList} from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Icon } from 'react-native-elements';
 import {DISHES} from '../shared/dishes';;
 import {COMMENTS} from '../shared/comments'
 
@@ -16,6 +16,14 @@ function RenderDish(props) {
                     <Text style={{margin: 10}}>
                         {dish.description}
                     </Text>
+                <Icon
+                raised
+                reverse
+                name={props.favorite ? 'heart' : 'heart-o'}
+                type='font-awesome'
+                color='#f50'
+                onPress={()=> props.favorite ? alert("Already Favorite") : props.onPress()}
+                />
                 </Card>
             );
         }
@@ -31,8 +39,8 @@ function RenderComment(props){
             return(
                <View key={index} style={{margin: 10}}>
                    <Text style={{fontSize:14}}>{item.comment}</Text>
-                    <Text style={{fontSize:12}}>{item.rating}</Text>
-                    <Text style={{fontSize:12}}>{'~~ ' + item.author}</Text>
+                    <Text style={{fontSize:12}}>{item.rating + '  stars'}</Text>
+                    <Text style={{fontSize:12}}>{item.date}{'~~ ' + item.author}</Text>
                </View>
             );
     }
@@ -58,14 +66,25 @@ class Dishdetail extends React.Component {
         super(props);
         this.state={
             dishes: DISHES,
-            comments: COMMENTS
+            comments: COMMENTS,
+            favorites: []
         }
     }
+
+    markedFavorite(dishId){
+        this.setState({
+            favorites: this.state.favorites.concat(dishId)
+        })
+    }
+
     render(){
         const dishId = this.props.route.params.dishId;
         return(
             <ScrollView>
-                <RenderDish dish={this.state.dishes[+dishId]} />
+                <RenderDish dish={this.state.dishes[+dishId]} 
+                favorite={this.state.favorites.some(el => el === dishId)}
+                onPress={()=> this.markedFavorite(dishId)}
+                />
                 <RenderComment comments={this.state.comments.filter((comment)=> comment.dishId === dishId)}/>
             </ScrollView>
         
