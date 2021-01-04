15782e78ff1d7e6a16955395853edf9f28db7f1e
Chetan Dhangar <chetansdhangar@gmail.com>
2021-01-04 22:30:04 +0530

    feat- gesture panResponder to  add favorite

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button} from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, PanResponder, Alert} from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 import {baseUrl} from '../shared/baseUrl';
@@ -8,30 +8,76 @@ import {postFavorite, postComment} from '../redux/ActionCreators';
 
 
 
-const mapStateToProps = state =>{
-    return{
-        dishes : state.dishes,
-        comments : state.comments,
-        favorites : state.favorites
-    }
-};
+    const mapStateToProps = state =>{
+        return{
+            dishes : state.dishes,
+            comments : state.comments,
+            favorites : state.favorites
+        }
+    };
 
-const mapDispatchToProps = dispatch => ({
-    postFavorite : (dishId) => dispatch(postFavorite(dishId)),
-    postComment : (dishId, rating, author, comment) => dispatch(postComment(dishId,rating,author,comment)),
-});
+    const mapDispatchToProps = dispatch => ({
+        postFavorite : (dishId) => dispatch(postFavorite(dishId)),
+        postComment : (dishId, rating, author, comment) => dispatch(postComment(dishId,rating,author,comment)),
+    });
 
-function RenderDish({
-    dish,
-    favorite,
-    markedFavorite,
-    toggleModal
-}) {
+    function RenderDish({
+        dish,
+        favorite,
+        markedFavorite,
+        toggleModal
+    }) {
 
+
+        handleViewRef = ref => this.view = ref;
+
+        const recognizeDrag = ({moveX, moveY, dx, dy}) => {
+            if(dx < -200){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder : (e, gestureState) =>{
+                return true;
+            },
+
+            onPanResponderGrant: () => {this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
+            onPanResponderEnd : (e, gestureState) =>{
+                console.log("Pan response End" , gestureState);
+                if(recognizeDrag(gestureState))
+                    Alert.alert(
+                        "Add Favorite",
+                        'Are you sure you wish to add'+ dish.name + 'as favorite dish',
+                        [
+                            {
+                                text : "Cancel",
+                                type: 'cancel',
+                                onPress: () => alert("You cancel the operation")
+                            },
+                            {
+                                text : 'OK',
+                                onPress : () => favorite ? alert("Already Favorite") : markedFavorite()
+                            }
+                        ],
+                        {cancelable : false}
+                    );
+                    return true ;
+               
+            }
+            
+        })
     
         if (dish != null) {
             return(
-                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+                 ref={this.handleViewRef} 
+
+                {...panResponder.panHandlers}
+                >
                 <Card
                 featuredTitle={dish.name}
                 image={{uri: baseUrl + dish.image}}>
