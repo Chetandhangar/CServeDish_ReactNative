a8fdba657a4995a25d1a93b3babc6c7f0ec5bdbf
Chetan Dhangar <chetansdhangar@gmail.com>
2021-01-07 10:53:32 +0530

    test- Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -5,6 +5,7 @@ import * as SecureStore from 'expo-secure-store';
 import * as Permissions from 'expo-permissions';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { baseUrl } from '../shared/baseUrl';
 
 class LoginTab extends Component {
@@ -154,10 +155,23 @@ class RegisterTab extends Component{
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri)
             }
         }
 
+    };
+
+    processImage  = async (imageUri) =>{
+        const processedIamge = ImageManipulator.manipulateAsync(
+            imageUri,
+            [
+                {resize : {width : 400}}
+            ],
+            {format :'png'}
+        );
+        console.log(processedIamge);
+        alert("Iamge Process")
+        this.setState({imageUrl : (await processedIamge).uri})
     }
     
     static navigationOptions = {
