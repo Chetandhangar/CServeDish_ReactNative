d1911542b1b078e55a53af27b65303f3a9520abd
Chetan Dhangar <chetansdhangar@gmail.com>
2021-01-08 09:21:15 +0530

    fix - LogBox warnings

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,LogBox} from 'react-native';
 import Main from './components/MainComponent';
 import {Provider} from 'react-redux'
 import {ConfigureStore} from './redux/configureStore';
@@ -8,8 +8,10 @@ import {PersistGate} from 'redux-persist/es/integration/react'
 import {Loading} from './components/LoadingComponent';
 
 const {persistor,store} = ConfigureStore();
-
+LogBox.ignoreLogs(['Warning: ...']);
+LogBox.ignoreAllLogs();
 export default function App() {
+  
   return (
     <Provider store={store}>
       <PersistGate 
